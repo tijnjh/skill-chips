62825b3f4f6abe5f3cfa70880097a97177c880d8
tijn <tijnjh@proton.me>
2025-03-27 00:36:47 +0100

    refactor: ✨ clean up SVG structure in SkillChip component

--- src/routes/v1/[skill]/SkillChip.svelte
+++ src/routes/v1/[skill]/SkillChip.svelte
@@ -55,48 +55,47 @@
 	}
 </script>
 
-<g rx="10" overflow="hidden">
-	<svg viewBox="0 0 {width} {height}" width="150" xmlns="http://www.w3.org/2000/svg">
-		<style>
-			@import url('https://fonts.googleapis.com/css2?family=Inter');
-		</style>
+<svg viewBox="0 0 {width} {height}" width="150" xmlns="http://www.w3.org/2000/svg">
+	<style>
+		@import url('https://fonts.googleapis.com/css2?family=Inter');
+	</style>
 
-		<defs>
-			<linearGradient id="skillGradient" x1="0" y1="0" x2="0" y2="1">
-				<stop offset="0%" stop-color={technology.color} stop-opacity="0.3" />
-				<stop offset="100%" stop-color={technology.color} stop-opacity="0.6" />
-			</linearGradient>
-		</defs>
+	<defs>
+		<linearGradient id="skillGradient" x1="0" y1="0" x2="0" y2="1">
+			<stop offset="0%" stop-color={technology.color} stop-opacity="0.3" />
+			<stop offset="100%" stop-color={technology.color} stop-opacity="0.6" />
+		</linearGradient>
+	</defs>
 
-		<rect {width} {height} overflow="hidden" fill="url(#skillGradient)" />
-		<text
-			x="65"
-			y="33"
-			fill={textColor}
-			font-family="Inter, system-ui"
-			font-size="25"
-			font-weight="600"
-		>
-			{technology.name}
-		</text>
+	<rect {width} {height} overflow="hidden" fill="url(#skillGradient)" />
+	<text
+		x="65"
+		y="33"
+		fill={textColor}
+		font-family="Inter, system-ui"
+		font-size="25"
+		font-weight="600"
+	>
+		{technology.name}
+	</text>
 
-		<text x="65" y="55" fill={textColor} opacity=".6" font-family="Inter, system-ui" font-size="20">
-			{level.charAt(0).toUpperCase() + level.slice(1)}
-		</text>
+	<text x="65" y="55" fill={textColor} opacity=".6" font-family="Inter, system-ui" font-size="20">
+		{level.charAt(0).toUpperCase() + level.slice(1)}
+	</text>
 
-		<rect height="9" y={height - 7} {width} fill="#ccc" />
-		<rect
-			height="9"
-			y={height - 7}
-			width={getBarWidthByLevel(level) * width}
-			fill={getBarColorByLevel(level)}
-		/>
+	<rect height="9" y={height - 7} {width} fill="#ccc" />
+	<rect
+		height="9"
+		y={height - 7}
+		width={getBarWidthByLevel(level) * width}
+		fill={getBarColorByLevel(level)}
+	/>
 
-		<svg width="40" x="12.5" height="40" y="15">
-			{@html technology.icon}
-		</svg>
+	<svg width="40" x="12.5" height="40" y="15">
+		{@html technology.icon}
 	</svg>
-</g>
+</svg>
+
 {#snippet bar(x: number, fill: string)}
 	<rect {x} y="15" width="10" height="40" {fill} />
 {/snippet}
