f12621dcb098c54e30e4c7a3b8510cf5c2429bdf
tijn <tijnjh@proton.me>
2025-03-26 15:02:35 +0100

    uncomment script tag in +page.svelte for browser redirection

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,4 @@
-<!-- <script lang="ts">
+<script lang="ts">
 	import { browser } from '$app/environment';
 	if (browser) window.location.href = 'https://github.com/tijnjh/skill-chips';
-</script> -->
+</script>
