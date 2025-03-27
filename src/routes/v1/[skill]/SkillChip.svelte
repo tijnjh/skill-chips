39dac1d8b16bd62c75556b044740b6f45fad27d3
tijn <tijnjh@proton.me>
2025-03-27 09:31:45 +0100

    refactor: 💄 tweak background gradient opacity

--- src/routes/v1/[skill]/SkillChip.svelte
+++ src/routes/v1/[skill]/SkillChip.svelte
@@ -60,8 +60,8 @@
 
 	<defs>
 		<linearGradient id="skillGradient" x1="0" y1="0" x2="0" y2="1">
-			<stop offset="0%" stop-color={technology.color} stop-opacity="0.3" />
-			<stop offset="100%" stop-color={technology.color} stop-opacity="0.6" />
+			<stop offset="0%" stop-color={technology.color} stop-opacity="0.1" />
+			<stop offset="100%" stop-color={technology.color} stop-opacity="0.3" />
 		</linearGradient>
 	</defs>
 
