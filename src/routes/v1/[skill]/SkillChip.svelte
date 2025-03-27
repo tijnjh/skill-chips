d7a792252f4f66990578a6f880b2b229de66b339
tijn <tijnjh@proton.me>
2025-03-27 15:00:27 +0100

    feat: ✨ update SkillChip component dimensions and improve level indicator rendering

--- src/routes/v1/[skill]/SkillChip.svelte
+++ src/routes/v1/[skill]/SkillChip.svelte
@@ -1,93 +1,124 @@
 <script lang="ts">
 	const { level, technology } = $props();
 
-	const width = 256;
-	const height = 80;
+	const width = 150;
+	const height = 50;
 
-	function getBarWidthByLevel(level: string) {
-		switch (level.toLowerCase()) {
-			case 'beginner':
-				return 0.25;
-			case 'learning':
-				return 0.5; // 50%
-			case 'intermediate':
-				return 0.75; // 75%
-			case 'advanced':
-				return 1; // 100%
-			default:
-				return 0;
-		}
+	function shadeColor(color: string, percent: number) {
+		var R = parseInt(color.substring(1, 3), 16);
+		var G = parseInt(color.substring(3, 5), 16);
+		var B = parseInt(color.substring(5, 7), 16);
+
+		R = parseInt(String((R * (100 + percent)) / 100));
+		G = parseInt(String((G * (100 + percent)) / 100));
+		B = parseInt(String((B * (100 + percent)) / 100));
+
+		R = R < 255 ? R : 255;
+		G = G < 255 ? G : 255;
+		B = B < 255 ? B : 255;
+
+		R = Math.round(R);
+		G = Math.round(G);
+		B = Math.round(B);
+
+		var RR = R.toString(16).length == 1 ? '0' + R.toString(16) : R.toString(16);
+		var GG = G.toString(16).length == 1 ? '0' + G.toString(16) : G.toString(16);
+		var BB = B.toString(16).length == 1 ? '0' + B.toString(16) : B.toString(16);
+
+		return '#' + RR + GG + BB;
 	}
 
-	function getBarColorByLevel(level: string) {
+	function getColorByLevel(level: string) {
 		switch (level.toLowerCase()) {
 			case 'beginner':
-				return 'oklch(0.795 0.184 86.047)';
+				return '#aceebb';
 			case 'learning':
-				return 'oklch(0.768 0.233 130.85)';
+				return '#4ac26b';
 			case 'intermediate':
-				return 'oklch(0.723 0.219 149.579)';
+				return '#2da44e';
 			case 'advanced':
-				return 'oklch(0.696 0.17 162.48)';
+				return '#116329';
 			default:
-				return '#ccc';
+				return '#eff2f5';
 		}
 	}
 </script>
 
-<svg viewBox="0 0 {width} {height}" width="150" xmlns="http://www.w3.org/2000/svg">
+<svg viewBox="0 0 {width} {height}" width="128" xmlns="http://www.w3.org/2000/svg">
 	<style>
-		@import url('https://fonts.googleapis.com/css2?family=Inter');
-
-		@media (prefers-color-scheme: dark) {
-			text {
-				fill: #fff;
-			}
-			svg {
-				background-color: #000;
-			}
-		}
-		@media (prefers-color-scheme: light) {
-			text {
-				fill: #000;
-			}
-
-			svg {
-				background-color: #ff;
-			}
+		* {
+			font-family:
+				-apple-system, BlinkMacSystemFont, 'Segoe UI', 'Noto Sans', Helvetica, Arial, sans-serif,
+				'Apple Color Emoji', 'Segoe UI Emoji';
+			color: #59636e;
 		}
 	</style>
 
-	<defs>
-		<linearGradient id="skillGradient" x1="0" y1="0" x2="0" y2="1">
-			<stop offset="0%" stop-color={technology.color} stop-opacity="0.1" />
-			<stop offset="100%" stop-color={technology.color} stop-opacity="0.3" />
-		</linearGradient>
-	</defs>
+	<rect
+		xmlns="http://www.w3.org/2000/svg"
+		x="0.5"
+		y="0.5"
+		rx="4.5"
+		height={height - 1}
+		stroke="#e4e2e2"
+		width={width - 1}
+		fill="#fffefe"
+		stroke-opacity="1"
+	/>
 
-	<rect {width} {height} fill="url(#skillGradient)" />
+	<g>
+		<circle cx="17.5" cy="17.5" r="5" fill={technology.color} />
 
-	<text x="70" y="35" font-family="Inter, system-ui" font-size="25" font-weight="600">
-		{technology.name}
-	</text>
+		<text x="25" y="21.5" fill="#59636e" font-size="12">
+			{technology.name}
+		</text>
+	</g>
 
-	<text x="70" y="58" opacity=".6" font-family="Inter, system-ui" font-size="20">
+	<text x="12.5" y="37.5" fill="#59636e" font-size="10">
 		{level.charAt(0).toUpperCase() + level.slice(1)}
 	</text>
 
-	<rect height="9" y={height - 7} {width} fill="#ccc" />
-	<rect
-		height="9"
-		y={height - 7}
-		width={getBarWidthByLevel(level) * width}
-		fill={getBarColorByLevel(level)}
-	/>
-
-	<svg width="45" x="12.5" height="40" y="17.5">
-		{@html technology.icon}
-	</svg>
+	{#if level}
+		{@const gap = 17.5}
+		{#if level === 'beginner'}
+			{@render levelIndicator(true)}
+			{@render levelIndicator(false, gap)}
+			{@render levelIndicator(false, 0, gap)}
+			{@render levelIndicator(false, gap, gap)}
+		{:else if level === 'learning'}
+			{@render levelIndicator(true)}
+			{@render levelIndicator(true, gap)}
+			{@render levelIndicator(false, 0, gap)}
+			{@render levelIndicator(false, gap, gap)}
+		{:else if level === 'intermediate'}
+			{@render levelIndicator(true)}
+			{@render levelIndicator(true, gap)}
+			{@render levelIndicator(true, 0, gap)}
+			{@render levelIndicator(false, gap, gap)}
+		{:else if level === 'advanced'}
+			{@render levelIndicator(true)}
+			{@render levelIndicator(true, gap)}
+			{@render levelIndicator(true, 0, gap)}
+			{@render levelIndicator(true, gap, gap)}
+		{/if}
+	{/if}
 </svg>
 
-{#snippet bar(x: number, fill: string)}
-	<rect {x} y="15" width="10" height="40" {fill} />
+{#snippet levelIndicator(active: boolean, x: number = 0, y: number = 0)}
+	{@const baseX = 110}
+	{@const baseY = 10}
+	{@const size = 12.5}
+	{@const fill = active ? getColorByLevel(level) : '#eff2f5'}
+	{@const stroke = shadeColor(fill, -6)}
+
+	<rect
+		rx="3"
+		x={baseX + x}
+		y={baseY + y}
+		width={size}
+		height={size}
+		{fill}
+		{stroke}
+		stroke-width=".5"
+	/>
 {/snippet}
